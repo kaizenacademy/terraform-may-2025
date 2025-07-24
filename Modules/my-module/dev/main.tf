a3d86c10d44874cacebb8d56d455465dacfb814b
kaizenacademy <kaizen.techacademy@gmail.com>
2025-07-24 01:40:39 +0000

    new files

---
+++ Modules/my-module/dev/main.tf
@@ -0,0 +1,21 @@
+provider aws {
+    region = var.region
+}
+
+module vpc {
+    source = "../vpc"
+    vpc_cidr = "10.0.0.0/16"
+    subnet1_cidr = "10.0.1.0/24"
+    environment = "dev"
+}
+
+module ec2 {
+    source = "../ec2"
+    ami_id = "ami-0cbbe2c6a1bb2ad63"
+    instance_type = "t2.micro"
+    subnet_id = module.vpc.subnet
+}
+
+variable region {
+    default = "us-east-1"
+}
